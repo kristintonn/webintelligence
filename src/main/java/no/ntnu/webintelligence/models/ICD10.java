fd1ac537e739ea32ee65582d00903a73c0d75f6e
Kristin Tønnesen <kristintonnesen@gmail.com>
2013-03-15 13:35:45 +0100

    ICD parsing

--- src/main/java/no/ntnu/webintelligence/models/ICD10.java
+++ src/main/java/no/ntnu/webintelligence/models/ICD10.java
@@ -5,15 +5,12 @@ import java.util.ArrayList;
 public class ICD10 {
 	private String id;
 	private String title;
-	private String description;
 	private ArrayList<String> synonyms;
 	
-	public ICD10(String id, String title, String description,
-			ArrayList<String> synonyms) {
+	public ICD10(String id, String title, ArrayList<String> synonyms) {
 		this.id = id;
 		this.title = title;
-		this.description = description;
-		synonyms = new ArrayList<String>();
+		this.synonyms = new ArrayList<String>();
 	}
 
 	public String getId() {
@@ -24,10 +21,10 @@ public class ICD10 {
 		return title;
 	}
 	
-	public String getDescription() {
-		return description;
-	}
-	
+        public void setTitle(String title){
+            this.title = title;
+        }
+        
 	public ArrayList<String> getSynonyms() {
 		return synonyms;
 	}
@@ -35,5 +32,11 @@ public class ICD10 {
 	public void addSynonym(String syn){
 		synonyms.add(syn);
 	}
+        
+        
+        @Override
+        public String toString(){
+            return id + ": " + title;
+        }
 	
 }
---
+++ src/main/java/no/ntnu/webintelligence/parsers/ParseICD.java
@@ -0,0 +1,133 @@
+package no.ntnu.webintelligence.parsers;
+
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.ontology.OntProperty;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import no.ntnu.webintelligence.models.ICD10;
+
+public class ParseICD {
+
+    private ArrayList<ICD10> parsedICDs;
+
+    public ParseICD() {
+        parsedICDs = new ArrayList<ICD10>();
+        
+        FileInputStream is;
+
+        try {
+            is = new FileInputStream("src/main/resources/icd10no.owl");
+            OntModel onto = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+            onto.read(is, "RDF/XML");
+
+            StmtIterator stit = onto.listStatements();
+            ICD10 icd10 = new ICD10(null, null, null);
+            while (stit.hasNext()) {
+
+                Statement stmt = stit.nextStatement();  // get next statement
+                Resource subject = stmt.getSubject();     // get the subject
+                Property predicate = stmt.getPredicate();   // get the predicate
+                RDFNode object = stmt.getObject();      // get the object
+
+                if (icd10.getId() == null) {
+                    icd10 = new ICD10(subject.getLocalName(), null, null);
+                } else {
+                    
+                }
+                
+                if (!icd10.getId().equals(subject.getLocalName())){
+                    parsedICDs.add(icd10);
+                }
+
+                if (predicate.getLocalName().equals("label") || predicate.getLocalName().equals("synonym")) {
+                    String value = object.toString();
+                    int i = value.indexOf("http");
+                    value = value.substring(0, i - 2);
+                   
+                    if (predicate.getLocalName().equals("label")){
+                        icd10.setTitle(value);
+                    }
+                    else if (predicate.getLocalName().equals("synonym")){
+                        icd10.addSynonym(value);
+                    }
+                }
+                
+
+
+                //System.out.print(" " + predicate.getLocalName() + " ");
+                if (object instanceof Resource) {
+                    //System.out.print(object.asResource());
+                } else {
+                    // object is a literal
+                    //System.out.print(" \"" + object.asLiteral() + "\"");
+                }
+
+                //System.out.println(" .");
+            }
+//            Iterator it = onto.listAllOntProperties();
+//            while (it.hasNext()) {
+//                Property p = (Property) it.next();
+//                properties.add(p);
+//            }
+//            System.out.println(properties);
+//            Iterator it2 = onto.listNamedClasses();
+//            while (it2.hasNext()) {
+//                OntClass ontClass = (OntClass) it2.next();
+//                String id = ontClass.getLocalName();
+//                System.out.println(id);
+//                String label = ontClass.getLabel(null);
+//                System.out.println(label);
+//
+//                System.out.println(ontClass.getCardinality(null));
+//                System.out.println(ontClass.getClass());
+//                System.out.println(ontClass.getComment(null));
+//                System.out.println(ontClass.getNameSpace());
+//                System.out.println(ontClass.getSubClass());
+//                System.out.println(ontClass.getSuperClass());
+//                System.out.println(ontClass.getRDFType());
+//                for (int i = 0; i<properties.size(); i++){
+//                    Property p = properties.get(i);
+//                    System.out.println(p.getLocalName());
+//                    RDFNode r = ontClass.getPropertyValue(p);
+//                    if (r != null){
+//                        System.out.println(r.toString());
+//                    }
+//                    
+//                    
+//                }
+//                ICD10 icd = new ICD10(id, label, null, null);
+//                Iterator it3 = ontClass.listDeclaredProperties();
+//                while (it3.hasNext()) {
+//                    OntProperty p2 = (OntProperty) it3.next();
+//                    if (ontClass.hasProperty(p2)) {
+//                        System.out.println(p2.getLocalName());
+//                        RDFNode r = p2.getPropertyValue(p2);
+//                        System.out.println(r);
+//                    }
+//
+//                }
+//            }
+//            System.out.println(onto.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public ArrayList<ICD10> getParsedICDs() {
+        return parsedICDs;
+    }
+    
+    public static void main(String[] args) {
+        ParseICD parseICD = new ParseICD();
+    }
+}
